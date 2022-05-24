d41a90bb30d38d41eba4e108bfc03732e531e2cd
“jiayaozhang” <jiayao.zhang@kaust.edu.sa>
2022-05-24 09:27:53 +0800

    add command buffers

---
+++ VulkanAPI_CPP/code_7_frame_command/code_7_frame_command/VulkanRenderer.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <array>
+
+#include "VulkanValidation.h"
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+
+	int init(GLFWwindow * newWindow);
+	void cleanup();
+
+	~VulkanRenderer();
+
+private:
+	GLFWwindow * window;
+
+	// Vulkan Components
+	// - Main
+	VkInstance instance;
+	VkDebugReportCallbackEXT callback;
+	struct {
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} mainDevice;
+	VkQueue graphicsQueue;
+	VkQueue presentationQueue;
+	VkSurfaceKHR surface;
+	VkSwapchainKHR swapchain;
+
+	std::vector<SwapchainImage> swapChainImages;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// - Pipeline
+	VkPipeline graphicsPipeline;
+	VkPipelineLayout pipelineLayout;
+	VkRenderPass renderPass;
+
+	// - Pools
+	VkCommandPool graphicsCommandPool;
+
+	// - Utility
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+
+	// Vulkan Functions
+	// - Create Functions
+	void createInstance();
+	void createDebugCallback();
+	void createLogicalDevice();
+	void createSurface();
+	void createSwapChain();
+	void createRenderPass();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+
+	// - Record Functions
+	void recordCommands();
+
+	// - Get Functions
+	void getPhysicalDevice();
+
+	// - Support Functions
+	// -- Checker Functions
+	bool checkInstanceExtensionSupport(std::vector<const char*> * checkExtensions);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	bool checkDeviceSuitable(VkPhysicalDevice device);
+
+	// -- Getter Functions
+	QueueFamilyIndices getQueueFamilies(VkPhysicalDevice device);
+	SwapChainDetails getSwapChainDetails(VkPhysicalDevice device);
+
+	// -- Choose Functions
+	VkSurfaceFormatKHR chooseBestSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &formats);
+	VkPresentModeKHR chooseBestPresentationMode(const std::vector<VkPresentModeKHR> presentationModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &surfaceCapabilities);
+
+	// -- Create Functions
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkShaderModule createShaderModule(const std::vector<char> &code);
+};
+
