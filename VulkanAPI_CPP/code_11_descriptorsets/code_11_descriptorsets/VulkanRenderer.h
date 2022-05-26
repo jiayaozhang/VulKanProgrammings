3e8b200ea0761fc610fea5d1b7d794930d70f6c8
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2022-05-26 17:47:48 +0800

    add descriptorsets

---
+++ VulkanAPI_CPP/code_11_descriptorsets/code_11_descriptorsets/VulkanRenderer.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <array>
+
+#include "Mesh.h"
+#include "VulkanValidation.h"
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+
+	int init(GLFWwindow * newWindow);
+
+	void updateModel(glm::mat4 newModel);
+
+	void draw();
+	void cleanup();
+
+	~VulkanRenderer();
+
+private:
+	GLFWwindow * window;
+
+	int currentFrame = 0;
+
+	// Scene Objects
+	std::vector<Mesh> meshList;
+
+	// Scene Settings
+	struct MVP {
+		glm::mat4 projection;
+		glm::mat4 view;
+		glm::mat4 model;
+	} mvp;
+
+	// Vulkan Components
+	// - Main
+	VkInstance instance;
+	VkDebugReportCallbackEXT callback;
+	struct {
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} mainDevice;
+	VkQueue graphicsQueue;
+	VkQueue presentationQueue;
+	VkSurfaceKHR surface;
+	VkSwapchainKHR swapchain;
+
+	std::vector<SwapchainImage> swapChainImages;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// - Descriptors
+	VkDescriptorSetLayout descriptorSetLayout;
+
+	VkDescriptorPool descriptorPool;
+	std::vector<VkDescriptorSet> descriptorSets;
+
+	std::vector<VkBuffer> uniformBuffer;
+	std::vector<VkDeviceMemory> uniformBufferMemory;
+
+	// - Pipeline
+	VkPipeline graphicsPipeline;
+	VkPipelineLayout pipelineLayout;
+	VkRenderPass renderPass;
+
+	// - Pools
+	VkCommandPool graphicsCommandPool;
+
+	// - Utility
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+
+	// - Synchronisation
+	std::vector<VkSemaphore> imageAvailable;
+	std::vector<VkSemaphore> renderFinished;
+	std::vector<VkFence> drawFences;
+
+	// Vulkan Functions
+	// - Create Functions
+	void createInstance();
+	void createDebugCallback();
+	void createLogicalDevice();
+	void createSurface();
+	void createSwapChain();
+	void createRenderPass();
+	void createDescriptorSetLayout();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+	void createSynchronisation();
+
+	void createUniformBuffers();
+	void createDescriptorPool();
+	void createDescriptorSets();
+
+	void updateUniformBuffer(uint32_t imageIndex);
+
+	// - Record Functions
+	void recordCommands();
+
+	// - Get Functions
+	void getPhysicalDevice();
+
+	// - Support Functions
+	// -- Checker Functions
+	bool checkInstanceExtensionSupport(std::vector<const char*> * checkExtensions);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	bool checkDeviceSuitable(VkPhysicalDevice device);
+
+	// -- Getter Functions
+	QueueFamilyIndices getQueueFamilies(VkPhysicalDevice device);
+	SwapChainDetails getSwapChainDetails(VkPhysicalDevice device);
+
+	// -- Choose Functions
+	VkSurfaceFormatKHR chooseBestSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &formats);
+	VkPresentModeKHR chooseBestPresentationMode(const std::vector<VkPresentModeKHR> presentationModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &surfaceCapabilities);
+
+	// -- Create Functions
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkShaderModule createShaderModule(const std::vector<char> &code);
+
+};
+
