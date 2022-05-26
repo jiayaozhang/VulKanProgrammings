c3249c6e3dad2eed4020edb30e266ab3e46265bc
Jiayao Zhang 张嘉瑶 <jiayao_zhang@outlook.com>
2022-05-26 17:46:24 +0800

    add push constants

---
+++ VulkanAPI_CPP/code_13_pushconstants/code_13_pushconstants/VulkanRenderer.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <array>
+
+#include "Mesh.h"
+#include "VulkanValidation.h"
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+
+	int init(GLFWwindow * newWindow);
+
+	void updateModel(int modelId, glm::mat4 newModel);
+
+	void draw();
+	void cleanup();
+
+	~VulkanRenderer();
+
+private:
+	GLFWwindow * window;
+
+	int currentFrame = 0;
+
+	// Scene Objects
+	std::vector<Mesh> meshList;
+
+	// Scene Settings
+	struct UboViewProjection {
+		glm::mat4 projection;
+		glm::mat4 view;
+	} uboViewProjection;
+
+	// Vulkan Components
+	// - Main
+	VkInstance instance;
+	VkDebugReportCallbackEXT callback;
+	struct {
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} mainDevice;
+	VkQueue graphicsQueue;
+	VkQueue presentationQueue;
+	VkSurfaceKHR surface;
+	VkSwapchainKHR swapchain;
+
+	std::vector<SwapchainImage> swapChainImages;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// - Descriptors
+	VkDescriptorSetLayout descriptorSetLayout;
+	VkPushConstantRange pushConstantRange;
+
+	VkDescriptorPool descriptorPool;
+	std::vector<VkDescriptorSet> descriptorSets;
+
+	std::vector<VkBuffer> vpUniformBuffer;
+	std::vector<VkDeviceMemory> vpUniformBufferMemory;
+
+	std::vector<VkBuffer> modelDUniformBuffer;
+	std::vector<VkDeviceMemory> modelDUniformBufferMemory;
+
+	//VkDeviceSize minUniformBufferOffset;
+	//size_t modelUniformAlignment;
+	//UboModel * modelTransferSpace;
+
+	// - Pipeline
+	VkPipeline graphicsPipeline;
+	VkPipelineLayout pipelineLayout;
+	VkRenderPass renderPass;
+
+	// - Pools
+	VkCommandPool graphicsCommandPool;
+
+	// - Utility
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+
+	// - Synchronisation
+	std::vector<VkSemaphore> imageAvailable;
+	std::vector<VkSemaphore> renderFinished;
+	std::vector<VkFence> drawFences;
+
+	// Vulkan Functions
+	// - Create Functions
+	void createInstance();
+	void createDebugCallback();
+	void createLogicalDevice();
+	void createSurface();
+	void createSwapChain();
+	void createRenderPass();
+	void createDescriptorSetLayout();
+	void createPushConstantRange();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+	void createSynchronisation();
+
+	void createUniformBuffers();
+	void createDescriptorPool();
+	void createDescriptorSets();
+
+	void updateUniformBuffers(uint32_t imageIndex);
+
+	// - Record Functions
+	void recordCommands(uint32_t currentImage);
+
+	// - Get Functions
+	void getPhysicalDevice();
+
+	// - Allocate Functions
+	void allocateDynamicBufferTransferSpace();
+
+	// - Support Functions
+	// -- Checker Functions
+	bool checkInstanceExtensionSupport(std::vector<const char*> * checkExtensions);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	bool checkDeviceSuitable(VkPhysicalDevice device);
+
+	// -- Getter Functions
+	QueueFamilyIndices getQueueFamilies(VkPhysicalDevice device);
+	SwapChainDetails getSwapChainDetails(VkPhysicalDevice device);
+
+	// -- Choose Functions
+	VkSurfaceFormatKHR chooseBestSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &formats);
+	VkPresentModeKHR chooseBestPresentationMode(const std::vector<VkPresentModeKHR> presentationModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &surfaceCapabilities);
+
+	// -- Create Functions
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkShaderModule createShaderModule(const std::vector<char> &code);
+
+};
+
---
+++ VulkanAPI_CPP/code_13_pushconstants/code_13_pushconstants/main.cpp
@@ -0,0 +1,74 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+
+#include "VulkanRenderer.h"
+
+GLFWwindow * window;
+VulkanRenderer vulkanRenderer;
+
+void initWindow(std::string wName = "Test Window", const int width = 800, const int height = 600)
+{
+	// Initialise GLFW
+	glfwInit();
+
+	// Set GLFW to NOT work with OpenGL
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(width, height, wName.c_str(), nullptr, nullptr);
+}
+
+int main()
+{
+	// Create Window
+	initWindow("Test Window", 800, 600);
+
+	// Create Vulkan Renderer instance
+	if (vulkanRenderer.init(window) == EXIT_FAILURE)
+	{
+		return EXIT_FAILURE;
+	}
+
+	float angle = 0.0f;
+	float deltaTime = 0.0f;
+	float lastTime = 0.0f;
+
+	// Loop until closed
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		float now = glfwGetTime();
+		deltaTime = now - lastTime;
+		lastTime = now;
+
+		angle += 10.0f * deltaTime;
+		if (angle > 360.0f) { angle -= 360.0f; }
+
+		glm::mat4 firstModel(1.0f);
+		glm::mat4 secondModel(1.0f);
+
+		firstModel = glm::translate(firstModel, glm::vec3(0.0f, 0.0f, -2.5f));
+		firstModel = glm::rotate(firstModel, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		secondModel = glm::translate(secondModel, glm::vec3(0.0f, 0.0f, -3.0f));
+		secondModel = glm::rotate(secondModel, glm::radians(-angle * 100), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		vulkanRenderer.updateModel(0, firstModel);
+		vulkanRenderer.updateModel(1, secondModel);
+
+		vulkanRenderer.draw();
+	}
+
+	vulkanRenderer.cleanup();
+
+	// Destroy GLFW window and stop GLFW
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
